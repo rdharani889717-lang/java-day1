e82efd6b68673c82841fb121743d6e6fa1923301
rdharani889717-lang <rdharani889717@gmail.com>
2026-02-07 11:32:00 +0530

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,20 @@
+class Datatype
+{
+    public static void main(String args[])
+    {
+        byte a=124;
+        short b=136;
+        int c=150000;
+        long d=100000000;
+        float e=1.2f;
+        double g=120.555;
+        char ch='d';
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(g);
+        System.out.println(ch);
+    }
+}
