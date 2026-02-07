b32fbd7b7a5420958559d06890d1df6e031bbbf9
rdharani889717-lang <rdharani889717@gmail.com>
2026-02-07 13:56:46 +0530

    Add files via upload

---
+++ Arthematic.java
@@ -0,0 +1,13 @@
+class Arthematic
+{
+    public static void main(String args[])
+    {
+        int a=12;
+        int b=10;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
